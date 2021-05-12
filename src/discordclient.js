da530f8ebe988bdc2dd9324c78a4854a39dd460e
Dawwa1 <71022309+Dawwa1@users.noreply.github.com>
2021-05-12 14:27:48 -0500

    Update discordclient.js

--- src/discordclient.js
+++ src/discordclient.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = ''; //process.env.DISCORD_TOKEN
+const token = process.env.DISCORD_TOKEN;
 
 try {
     client.login(token);
